0b90ae96359fceb8c312e7bc1186dee25f2aaf50
Rafael <rafael-brm@hotmail.com>
2025-07-10 00:58:14 -0300

    minor refactor

--- mirrors/src/main.js
+++ mirrors/src/main.js
@@ -7,15 +7,11 @@ import { MainSimulation2 } from './simulation2/mainSimulation2.js';
 import { sceneEntities } from './config/scenesConfig.js';
 import { generalConfig } from './config/generalConfig.js';
 
-// Create and initialize the simulation
-// const simulation = new MainSimulation({
-//     canvas: document.getElementById('mirror-canvas'),
-//     ...sceneEntities
-// });
+const svgCanvas = document.getElementById('mirror-canvas');
 
 const simulation = new MainSimulation2({
     sceneEntities: sceneEntities,
-    svgCanvas: document.getElementById('mirror-canvas'),
+    svgCanvas: svgCanvas,
     generalConfig: generalConfig
 });
 
